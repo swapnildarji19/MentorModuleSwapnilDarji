9cbdb5a01420e6c653bb6e2eeeaab79589aa6fb8
Swapnil Darji <Swapnil.darji@somaiya.edu>
2021-03-19 22:18:53 +0530

    WebAPI

---
+++ WebApplication1/WebApplication1/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using WebApplication1.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost, Route("login")]
+        public IActionResult Login([FromBody]LoginModel user)
+        {
+            if (user == null)
+                return BadRequest("Invalid client request");
+
+            if(user.UserName == "swapnil" && user.Password == "qwerty")
+            {
+                return Ok();
+            }
+            return Unauthorized();
+        }
+    }
+}
