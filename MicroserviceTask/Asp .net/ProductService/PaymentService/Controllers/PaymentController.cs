88adf31292a891d1cd854e27f2334aedf11a9fa9
Swapnil Darji <Swapnil.darji@somaiya.edu>
2021-04-12 01:50:07 +0530

    Created two Microservices

---
+++ MicroserviceTask/Asp .net/ProductService/PaymentService/Controllers/PaymentController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PaymentService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        // GET: api/<PaymentController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Random rand = new Random();
+            int randomLessThan100 = rand.Next(100);
+
+            if (1 == (randomLessThan100 & 1))
+            {
+                return Ok();
+            }
+            return Unauthorized();
+        }
+
+        // GET api/<PaymentController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PaymentController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PaymentController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PaymentController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MicroserviceTask/Asp .net/ProductService/ProductService/Controllers/ProductController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProductService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        // GET: api/<ProductController>
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            Product product1 = new Product()
+            {
+                Id = 101,
+                Name = "Product 1",
+                Availability = 1,
+                Description = "Product 1 Description"
+            };
+
+            Product product2 = new Product()
+            {
+                Id = 102,
+                Name = "Product 2",
+                Availability = 6,
+                Description = "Product 2 Description"
+            };
+
+            Product product3 = new Product()
+            {
+                Id = 103,
+                Name = "Product 3",
+                Availability = 5,
+                Description = "Product 3 Description"
+            };
+
+            Product product4 = new Product()
+            {
+                Id = 104,
+                Name = "Product 4",
+                Availability = 34,
+                Description = "Product 4 Description"
+            };
+
+            List<Product> products = new List<Product>(2);
+            products.Add(product1);
+            products.Add(product2);
+            products.Add(product3);
+            products.Add(product4);
+
+            return products;
+        }
+
+        // GET api/<ProductController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ProductController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ProductController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
